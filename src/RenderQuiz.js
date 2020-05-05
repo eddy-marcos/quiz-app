26929ae1c57440ec26e7aac5fb602d4c4b257214
Eddy Marcos <emarcos@rpa.com>
2020-05-04 23:18:03 -0600

    converted to class comp

--- src/RenderQuiz.js
+++ src/RenderQuiz.js
@@ -1,64 +1,81 @@
 import React from 'react';
 
-const handleRadioCheck = (radioTarget, answer) => {
-  let radioValue = radioTarget.value;
-  let correctAnswer = answer;
+class RenderQuiz extends React.Component {
 
-  if(correctAnswer === radioValue) {
-    // console.log('true');
-    return true;
+   handleRadioCheck(radioTarget, answer) {
+    let radioValue = radioTarget.value;
+    let correctAnswer = answer;
+    // radioTarget.setAttribute('checked', true)
+
+  //   if (e.target && e.target.nodeName === "LI") {
+  //   let elements = document.getElementById('main-li').children;
+  //   for (let i = 0; i < elements.length; ++i) {
+  //       elements[i].classList.remove("active");
+  //   }
+  //   e.target.classList.add("active");
+  // }
+
+
+    if(correctAnswer === radioValue) {
+      // console.log('true');
+      return true;
+    }
+    // console.log('false');
+    return false;
   }
-  // console.log('false');
-  return false;
-}
 
-const submit = (event, answer) => {
-    event.preventDefault();
-    // console.log('answer ', answer);
-    // need to get the checked input value
-    console.log(event.target);
-}
+   handleSubmit(event, answer) {
+      event.preventDefault();
+      // console.log('answer ', answer);
+      // need to get the checked input value
+      console.log(this.element.checked);
+  }
 
-const RenderQuiz = (props) => {
-  return (
-    props.state.map(d => {
-        return(
-          <div key={d.question}>
-            <p>{d.question} {d.description}</p>
+  render() {
+    return (
+      this.props.state.map(d => {
+          return(
+            <div key={d.question}>
+              <p>{d.question} {d.description}</p>
 
-            <form onSubmit={e => submit(e, d.correctAnswer)}>
-                 <div>
-                  <input type="radio" id="option1" name={d.question} value="a"
-                    onClick={(e)=>handleRadioCheck(e.target, d.correctAnswer)}
-                    defaultChecked/>
-                    <label htmlFor="option1">{d.options['a']}</label>
-                </div>
-                <div>
-                  <input type="radio" id="option2" name={d.question} value="b"
-                    onClick={(e)=>handleRadioCheck(e.target, d.correctAnswer)}
-                    />
-                    <label htmlFor="option2">{d.options['b']}</label>
-                </div>
-                <div>
-                  <input type="radio" id="option3" name={d.question} value="c"
-                    onClick={(e)=>handleRadioCheck(e.target, d.correctAnswer)}
-                    />
-                    <label htmlFor="option3">{d.options['c']}</label>
-                </div>
-                <div>
-                  <input type="radio" id="option4" name={d.question} value="d"
-                    onClick={(e)=>handleRadioCheck(e.target, d.correctAnswer)}
-                    />
-                    <label htmlFor="option4">{d.options['d']}</label>
-                </div>
-                <div>
-                  <input type="submit" value="Submit"/>
-                </div>
-            </form>
-      </div>
-        )
-      })
-  );
+              <form onSubmit={e => this.handleSubmit(e, d.correctAnswer)}>
+                   <div>
+                    <input type="radio" id="option1" name={d.question} value="a"
+                      onClick={(e)=>this.handleRadioCheck(e.target, d.correctAnswer)}
+                      ref={e=>this.element = e}
+                      />
+                      <label htmlFor="option1">{d.options['a']}</label>
+                  </div>
+                  <div>
+                    <input type="radio" id="option2" name={d.question} value="b"
+                      onClick={(e)=>this.handleRadioCheck(e.target, d.correctAnswer)}
+                      ref={e=>this.element = e}
+                      />
+                      <label htmlFor="option2">{d.options['b']}</label>
+                  </div>
+                  <div>
+                    <input type="radio" id="option3" name={d.question} value="c"
+                      onClick={(e)=>this.handleRadioCheck(e.target, d.correctAnswer)}
+                      ref={e=>this.element = e}
+                      />
+                      <label htmlFor="option3">{d.options['c']}</label>
+                  </div>
+                  <div>
+                    <input type="radio" id="option4" name={d.question} value="d"
+                      onClick={(e)=>this.handleRadioCheck(e.target, d.correctAnswer)}
+                      ref={e=>this.element = e}
+                      />
+                      <label htmlFor="option4">{d.options['d']}</label>
+                  </div>
+                  <div>
+                    <input type="submit" value="Submit"/>
+                  </div>
+              </form>
+        </div>
+          )
+        })
+    );
+  }
 }
 
 export default RenderQuiz;
