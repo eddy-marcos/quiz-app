953884c10a9c0f49ca2e6d59f5f90860987f6fad
Eddy Marcos <emarcos@rpa.com>
2020-05-06 15:42:50 -0600

    convered quiz to functional comp back

--- src/RenderQuiz.js
+++ src/RenderQuiz.js
@@ -1,71 +1,69 @@
 import React from 'react';
 
-class RenderQuiz extends React.Component {
+const RenderQuiz = (props) => {
 
-   handleRadioCheck(option) {
-    this.props.updateChecked(option);
+   const handleRadioCheck = (option) => {
+    props.updateChecked(option);
   }
 
-   handleSubmit(event, answer) {
+   const handleSubmit = (event, answer) => {
       event.preventDefault();
 
-      if (this.props.currentChecked === answer) {
+      if (props.currentChecked === answer) {
         // console.log('true');
-        this.props.updateResult('Excelente! :)');
+        props.updateResult('Excelente! :)');
         return true;
       }
 
       // console.log('false');
-      this.props.updateResult('Intenta de nuevo.');
+      props.updateResult('Intenta de nuevo.');
       return false;
   }
 
-  render(props) {
     return (
-      this.props.state.map(d => {
+      props.state.map(d => {
           return(
             <div key={d.question}>
               <p>{d.question} {d.description}</p>
 
-              <form onSubmit={e => this.handleSubmit(e, d.correctAnswer)}>
+              <form onSubmit={e => handleSubmit(e, d.correctAnswer)}>
                    <div>
                     <input type="radio" id="option1" name={d.question} value={d.question}
-                      checked={this.props.currentChecked === "optionA"}
-                      onChange={e => this.handleRadioCheck("optionA")}
+                      checked={props.currentChecked === "optionA"}
+                      onChange={e => handleRadioCheck("optionA")}
                       />
                       <label htmlFor="option1">{d.options['a']}</label>
                   </div>
                   <div>
                     <input type="radio" id="option2" name={d.question} value={d.question}
-                      checked={this.props.currentChecked === "optionB"}
-                      onChange={e => this.handleRadioCheck("optionB")}
+                      checked={props.currentChecked === "optionB"}
+                      onChange={e => handleRadioCheck("optionB")}
                       />
                       <label htmlFor="option2">{d.options['b']}</label>
                   </div>
                   <div>
                     <input type="radio" id="option3" name={d.question} value={d.question}
-                      checked={this.props.currentChecked === "optionC"}
-                      onChange={e => this.handleRadioCheck("optionC")}
+                      checked={props.currentChecked === "optionC"}
+                      onChange={e => handleRadioCheck("optionC")}
                       />
                       <label htmlFor="option3">{d.options['c']}</label>
                   </div>
                   <div>
                     <input type="radio" id="option4" name={d.question} value={d.question}
-                      checked={this.props.currentChecked === "optionD"}
-                      onChange={e => this.handleRadioCheck("optionD")}
+                      checked={props.currentChecked === "optionD"}
+                      onChange={e => handleRadioCheck("optionD")}
                       />
                       <label htmlFor="option4">{d.options['d']}</label>
                   </div>
                   <div>
                     <input type="submit" value="Submit"/>
                 </div>
-                  <span className="result">{this.props.currentResult}</span>
+                  <span className="result">{props.currentResult}</span>
               </form>
         </div>
           )
         })
     );
   }
-}
 
 export default RenderQuiz;
--- src/index.js
+++ src/index.js
@@ -17,6 +17,50 @@ const data = [
       "d": "question1 option d"
     },
     "correctAnswer" : "optionA"
+  },
+  {
+    "question": 2,
+    "description": "this is question 2",
+    "options": {
+      "a": "question2 option a",
+      "b": "question2 option b",
+      "c": "question2 option c",
+      "d": "question2 option d"
+    },
+    "correctAnswer" : "optionB"
+  },
+  {
+    "question": 3,
+    "description": "this is question 3",
+    "options": {
+      "a": "question3 option a",
+      "b": "question3 option b",
+      "c": "question3 option c",
+      "d": "question3 option d"
+    },
+    "correctAnswer" : "optionC"
+  },
+  {
+    "question": 4,
+    "description": "this is question 4",
+    "options": {
+      "a": "question4 option a",
+      "b": "question4 option b",
+      "c": "question4 option c",
+      "d": "question4 option d"
+    },
+    "correctAnswer" : "optionD"
+  },
+  {
+    "question": 5,
+    "description": "this is question 5",
+    "options": {
+      "a": "question5 option a",
+      "b": "question5 option b",
+      "c": "question5 option c",
+      "d": "question5 option d"
+    },
+    "correctAnswer" : "optionC"
   }
 ]
 
