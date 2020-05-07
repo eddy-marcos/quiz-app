156047cef23f3624bc2e5432ab525cb58755a96f
Eddy Marcos <emarcos@rpa.com>
2020-05-07 17:45:01 -0600

    Updates

---
+++ precache-manifest.566d447d17b9907b934f407acbcb7533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d75645d171d1b4d96b3f7055a393446",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "026b848b42cdfc8a83ba",
+    "url": "/quiz-app/static/css/main.f2795c9e.chunk.css"
+  },
+  {
+    "revision": "a9202a7e05e64a855b29",
+    "url": "/quiz-app/static/js/2.4a041ac7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.4a041ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026b848b42cdfc8a83ba",
+    "url": "/quiz-app/static/js/main.b1e71e76.chunk.js"
+  },
+  {
+    "revision": "5f29021a1b2f89e2c0b7",
+    "url": "/quiz-app/static/js/runtime-main.0dece909.js"
+  }
+]);
